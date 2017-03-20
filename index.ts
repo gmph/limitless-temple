b74d4cfa60e195917070ae3755ec782a5fcdcb91
Graham Macphee <hi@grahammacphee.co.uk>
2017-03-20 20:16:20 +0000

    Added chart config

--- index.ts
+++ index.ts
@@ -28,10 +28,46 @@ app.listen(app.get('port'), () => {
   console.log('App running on port', app.get('port'));
 });
 
+let chartConfig = {
+    type: 'horizontalBar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+}
 
 function getChart(): any {
     let chartNode = new ChartjsNode(600, 600);
-    return chartNode.drawChart().then(() => {
+    return chartNode.drawChart(chartConfig).then(() => {
         return chartNode.getImageBuffer('image/png');
     })
 }
