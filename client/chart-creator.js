28af8bfcddf1bcc158b958a37077571694729e2d
Graham Macphee <hi@grahammacphee.co.uk>
2017-03-21 12:39:06 +0000

    Fix client JS serving (was ignored)

---
+++ client/chart-creator.js
@@ -0,0 +1,17 @@
+$('#create-chart').click(function(){
+    var labels = [],
+        values = [];
+    $('.chart-form .label').each(function(i, label){
+        if (label.value && label.value.length) {
+            labels.push(label.value);
+            var value = $('.chart-form .value').get(i);
+            values.push(value.value && value.value.length ? value.value : 0);
+        }
+    });
+    $('#chart-image').attr('src',getChartUrlFromParams(labels, values));
+});
+
+function getChartUrlFromParams(labels, values){
+    var chartBaseUrl = "http://localhost:5000/chart";
+    return chartBaseUrl + "?labels=" + labels.join(',') + "&values=" + values.join(',');
+}
